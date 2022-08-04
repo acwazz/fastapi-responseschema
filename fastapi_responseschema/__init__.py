18d6a5a5c7df0f0c06c35e97cd2fc9f8d27a0950
Emanuele Addis <ustarjem.acwazz@gmail.com>
2022-08-04 15:47:57 +0200

    [ADD] Bump version

--- fastapi_responseschema/__init__.py
+++ fastapi_responseschema/__init__.py
@@ -3,7 +3,7 @@ from .routing import respond, SchemaAPIRoute
 from .helpers import wrap_app_responses, wrap_error_responses
 
 
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 
 __all__ = [
--- tests/test_fastapi_responseschema.py
+++ tests/test_fastapi_responseschema.py
@@ -2,4 +2,4 @@ from fastapi_responseschema import __version__
 
 
 def test_version():
-    assert __version__ == '1.0.1'
+    assert __version__ == '1.1.0'
