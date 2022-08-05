fc1a04b5eaccca3644d0202309cd5f67e2804168
Emanuele Addis <ustarjem.acwazz@gmail.com>
2022-08-05 17:49:19 +0200

    [ADD] Tests for #1

---
+++ tests/test_response_model_compatibility.py
@@ -0,0 +1,51 @@
+import string
+from fastapi import FastAPI, APIRouter
+from fastapi.testclient import TestClient
+from fastapi_responseschema import SchemaAPIRoute, wrap_app_responses
+from pydantic import BaseModel
+from .common import SimpleResponseSchema
+
+
+class RModel(BaseModel):
+    id: str
+    name: str
+
+
+class Route(SchemaAPIRoute):
+    response_schema = SimpleResponseSchema
+
+
+app = FastAPI()
+
+
+wrap_app_responses(app, Route)
+
+
+@app.get("/model-exclude", response_model=RModel, response_model_exclude={"name"})
+def wrapped_call_first():
+    return {"id": 1, "name": "hiddenfield"}
+
+
+@app.get("/model-filters", response_model=RModel)
+def wrapped_call_first():
+    return {"id": 1, "name": "hiddenfield", "hidden": True}
+
+
+client = TestClient(app)
+
+
+def test_response_model_exclude_preserved():
+    r = client.get("/model-exclude")
+    resp = r.json()
+    assert not resp.get("error")
+    assert resp.get("data").get("id") == 1
+    assert not resp.get("data").get("name")
+
+
+def test_response_model_filters():
+    r = client.get("/model-filters")
+    resp = r.json()
+    assert not resp.get("error")
+    assert resp.get("data").get("id") == 1
+    assert resp.get("data").get("name")
+    assert not resp.get("data").get("hidden")
